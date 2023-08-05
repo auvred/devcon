c318a7bd73790f03c03c7e36d2267ef82bad47ec
auvred <aauvred@gmail.com>
2023-08-05 23:54:50 +0300

    feat: add base setup

---
+++ setup-asdf.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.12.0
+echo ". $HOME/.asdf/asdf.sh" >> $HOME/.bashrc
+sudo /bin/bash -c 'echo ". $HOME/.asdf/asdf.sh" >> /root/.bashrc'
+. $HOME/.asdf/asdf.sh
+
+NEOVIM_VERSION=0.9.1
+NODEJS_VERSION=18.17.0
+
+asdf plugin add neovim 
+asdf install neovim $NEOVIM_VERSION 
+asdf global neovim $NEOVIM_VERSION 
+
+asdf plugin add nodejs 
+asdf install nodejs $NODEJS_VERSION 
+asdf global nodejs $NODEJS_VERSION
---
+++ setup-stuff.sh
@@ -0,0 +1,8 @@
+. $HOME/.asdf/asdf.sh
+
+sudo locale-gen en_US.UTF-8 
+curl -o- https://raw.githubusercontent.com/auvred/dotfiles/master/install.sh | bash
+
+echo ". $HOME/.asdf/asdf.sh" >> $HOME/.zshrc
+
+echo "store-dir=~/.pnpm-store" > ~/.npmrc 
